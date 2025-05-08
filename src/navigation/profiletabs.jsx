26f00a93fcb2af2815b533ae64b0f623388f3eef
neel <niladrichaturvedi469@gmail.com>
2025-05-08 11:37:39 +0530

    feat: add material top tab navigator for ProfileTabs component

---
+++ src/navigation/profiletabs.jsx
@@ -0,0 +1,9 @@
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+function ProfileTabs() {
+  return <Tab.Navigator></Tab.Navigator>;
+}
+
+export default ProfileTabs;
