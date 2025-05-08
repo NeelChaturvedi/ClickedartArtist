a79700872dd4c089638b9ed9345a0a9d12f62c53
bhanu-sh <bhanu1234sharma@gmail.com>
2025-05-08 12:08:20 +0530

    feat: update Profile component to display user images and details dynamically

--- src/Screens/main/Profile/index.jsx
+++ src/Screens/main/Profile/index.jsx
@@ -3,20 +3,22 @@ import {Pressable, SafeAreaView, ScrollView, Text, View} from 'react-native';
 import React from 'react';
 import {style} from './styles';
 import {Image} from 'moti';
+import { useUserStore } from '../../../store/auth';
 
 const Profile = () => {
+  const { user } = useUserStore()
   return (
     <SafeAreaView style={style.background}>
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
         <View>
           <Image
             style={style.coverImage}
-            source={require('../../../assets/images/onboarding.png')}
+            source={user?.coverImage ? {uri: user?.coverImage} : require('../../../assets/images/onboarding.png')} //change placeholder later
           />
           <View style={style.profileDiv}>
             <Image
               style={style.profileImage}
-              source={require('../../../assets/images/onboarding.png')}
+              source={user?.profileImage ? {uri: user?.profileImage} : require('../../../assets/images/onboarding.png')} //change placeholder later
             />
             <Pressable style={style.edit}>
               <Image
@@ -27,8 +29,8 @@ const Profile = () => {
           </View>
         </View>
         <View style={style.userDetails}>
-          <Text style={style.userName}>Bhanu Sharma</Text>
-          <Text style={style.userAddress}>Lucknow, India</Text>
+          <Text style={style.userName}>{user.firstName || ''} {user.lastName || ''}</Text>
+          <Text style={style.userAddress}>{user?.shippingAddress.city}, {user?.shippingAddress.country}</Text>
         </View>
       </ScrollView>
       <View></View>
