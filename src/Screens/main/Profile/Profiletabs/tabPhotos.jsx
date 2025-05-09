e1751a01ffec0cf504733e2dcb0cf1482de33263
neel <niladrichaturvedi469@gmail.com>
2025-05-09 13:55:18 +0530

    feat: refactor TabPhotos component to use View instead of FlatList for improved layout and readability

--- src/Screens/main/Profile/Profiletabs/tabPhotos.jsx
+++ src/Screens/main/Profile/Profiletabs/tabPhotos.jsx
@@ -1,59 +1,58 @@
-import {
-  View,
-  Text,
-  FlatList,
-  Image,
-  StyleSheet,
-} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import React from 'react';
 
 const dummyImages = [
-  { id: '1', source: require('../../../../assets/images/onboarding.png'), title: 'Image 1' },
-  { id: '2', source: require('../../../../assets/images/onboarding.png'), title: 'Image 2' },
-  { id: '3', source: require('../../../../assets/images/onboarding.png'), title: 'Image 3' },
+  {
+    id: '1',
+    source: require('../../../../assets/images/onboarding.png'),
+    title: 'Image 1',
+  },
+  {
+    id: '2',
+    source: require('../../../../assets/images/onboarding.png'),
+    title: 'Image 2',
+  },
+  {
+    id: '3',
+    source: require('../../../../assets/images/onboarding.png'),
+    title: 'Image 3',
+  },
 ];
 
 const TabPhotos = () => {
-  const renderItem = ({ item }) => (
-    <View style={styles.imageBorder}>
-      <Image style={styles.image} source={item.source} />
-      <View style={styles.imageDetails}>
-        <Text style={styles.imageText}>{item.title}</Text>
-      </View>
-    </View>
-  );
-
   return (
-    <FlatList
-      numColumns={2}
-      data={dummyImages}
-      keyExtractor={(item) => item.id}
-      renderItem={renderItem}
-      contentContainerStyle={styles.listContainer}
-      showsHorizontalScrollIndicator={false}
-    />
+    <View style={styles.container}>
+      {dummyImages.map(item => (
+        <View key={item.id} style={styles.imageBorder}>
+          <Image style={styles.image} source={item.source} />
+          <View style={styles.imageDetails}>
+            <Text style={styles.imageText}>{item.title}</Text>
+          </View>
+        </View>
+      ))}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  listContainer: {
-    paddingBottom: 90,
-    flexDirection: 'column',
-    rowGap: 10,
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     paddingHorizontal: 10,
+    gap: 10,
+    paddingBottom: 90,
   },
   imageBorder: {
     paddingTop: 8,
     paddingBottom: 10,
     paddingHorizontal: 10,
-    width: 170,
+    width: '47%',
     height: 190,
     justifyContent: 'space-between',
     backgroundColor: '#1A1A1A',
     borderWidth: 1,
     borderColor: '#fff',
     borderRadius: 10,
-    marginRight: 10,
   },
   image: {
     resizeMode: 'cover',
