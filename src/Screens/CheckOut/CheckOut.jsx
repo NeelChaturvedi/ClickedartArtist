27194491cb720b6bb8d8961020cbb3f0b20deb3f
neel <niladrichaturvedi469@gmail.com>
2025-05-22 15:00:46 +0530

    feat: implement CheckOut screen with delivery details layout and styles

--- src/Screens/CheckOut/CheckOut.jsx
+++ src/Screens/CheckOut/CheckOut.jsx
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
 import React from 'react';
+import {styles} from './styles';
 
 const CheckOut = () => {
   return (
-    <View>
-      <Text>CheckOut</Text>
-    </View>
+    <SafeAreaView style={styles.background}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Delivery Details</Text>
+        <ScrollView></ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
---
+++ src/Screens/CheckOut/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    background:{
+        flex: 1,
+        width: '100%',
+        backgroundColor: 'black',
+    },
+    container:{
+        padding: 20,
+    },
+    header:{
+        fontFamily: 'Outfit-bold',
+        fontSize: 20,
+        color:'white',
+    },
+});
