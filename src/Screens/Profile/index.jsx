47ac435a7b284726ae26123bb07a714485cb13cf
neel <niladrichaturvedi469@gmail.com>
2025-05-30 10:17:58 +0530

    feat: Refactor Profile screen to improve data fetching and UI structure

--- src/Screens/Profile/index.jsx
+++ src/Screens/Profile/index.jsx
@@ -1,23 +1,22 @@
 /* eslint-disable react-native/no-inline-styles */
 import {
-  View,
-  Text,
-  StyleSheet,
-  RefreshControl,
   Pressable,
+  RefreshControl,
+  ScrollView,
+  Text,
+  View,
   Share,
   Alert,
+  ActivityIndicator,
   ToastAndroid,
   TouchableOpacity,
 } from 'react-native';
-import {MaterialTabBar, Tabs} from 'react-native-collapsible-tab-view';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Image} from 'moti';
 import {useUserStore} from '../../store/auth';
 import TabPhotos from '../Profiletabs/TabPhotos';
 import TabCatalogues from '../Profiletabs/TabCatalogue';
 import TabBlogs from '../Profiletabs/TabBlogs';
-import {useTheme} from 'src/themes/useTheme';
-import React, {useCallback, useEffect, useState} from 'react';
-import {Image, MotiView, ScrollView} from 'moti';
 import {useNavigation} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -26,38 +25,12 @@ import SlideUpModal from '../../components/SlideupModal';
 import ImagePicker from 'react-native-image-crop-picker';
 import axios from 'axios';
 import {API_URL} from '@env';
+import {useTheme} from 'src/themes/useTheme';
 import {useMemo} from 'react';
 import {createProfileStyles} from './styles';
-import {usePhotosStore} from 'src/store/photos';
-import {useAnalyticsStore} from 'src/store/photographerAnalytics';
-import {useCataloguesStore} from 'src/store/catalogues';
-import {useBlogsStore} from 'src/store/blogs';
-import {usePendingPhotosStore} from 'src/store/pendingPhotos';
-import {usePendingBlogsStore} from 'src/store/pendingBlogs';
-import ProfileSkeleton from './Loader';
 
 const Profile = () => {
-  const {user, loading: userLoading, fetchUserFromToken} = useUserStore();
-  const {
-    loading: photosLoading,
-    fetchPhotos,
-    pageNumber: photosPageNumber,
-    pageCount: photosPageCount,
-    fetchMorePhotos,
-  } = usePhotosStore();
-  const {stats, loading: statsLoading, fetchStats} = useAnalyticsStore();
-  const {loading: cataloguesLoading, fetchCatalogues} = useCataloguesStore();
-  const {blogs, loading: blogsLoading, fetchBlogs} = useBlogsStore();
-  const {
-    pendingPhotos,
-    loading: pendingPhotosLoading,
-    fetchPendingPhotos,
-  } = usePendingPhotosStore();
-  const {
-    pendingBlogs,
-    loading: pendingBlogsLoading,
-    fetchPendingBlogs,
-  } = usePendingBlogsStore();
+  const {user, fetchUserFromToken} = useUserStore();
 
   const theme = useTheme();
   const style = useMemo(() => createProfileStyles(theme), [theme]);
@@ -67,25 +40,18 @@ const Profile = () => {
   const [slideUp, setSlideUp] = useState(false);
 
   const [activeTab, setActiveTab] = useState('photos');
+  const [stats, setStats] = useState({});
+  const [photos, setPhotos] = useState([]);
+  const [pendingPhotos, setPendingPhotos] = useState([]);
+  const [catalogues, setCatalogues] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [pendingBlogs, setPendingBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
   const [profileUploading, setProfileUploading] = useState(false);
   const [fullBio, setFullBio] = useState(null);
 
-  const onScroll = event => {
-    console.log('scrolling');
-    const {layoutMeasurement, contentOffset, contentSize} = event.nativeEvent;
-    console.log(
-      'Scroll Position:',
-      layoutMeasurement.height + contentOffset.y,
-      'Content Size:',
-      contentSize.height,
-    );
-    if (layoutMeasurement.height + contentOffset.y >= contentSize.height - 20) {
-      if (!photosLoading && photosPageNumber < photosPageCount) {
-        fetchMorePhotos(user._id);
-      }
-    }
-  };
+  console.log('fullbio:', fullBio);
 
   const onShare = async () => {
     try {
@@ -215,211 +181,238 @@ const Profile = () => {
     setActiveTab(tab);
   };
 
-  const fetchAllData = async () => {
-    console.log('Fetching all data for user:', user._id);
-    await fetchPhotos(user._id);
-    await fetchStats(user._id);
-    await fetchCatalogues(user._id);
-    await fetchBlogs(user._id);
-    await fetchPendingPhotos(user._id);
-    await fetchPendingBlogs(user._id);
-  };
+  const fetchAllData = useCallback(async () => {
+    try {
+      setLoading(true);
 
-  const onRefresh = async () => {
+      const [
+        statsRes,
+        photosRes,
+        pendingPhotosRes,
+        cataloguesRes,
+        blogsRes,
+        pendingBlogsRes,
+      ] = await Promise.allSettled([
+        api.get(
+          `/photographeranalytics/get-photographer-analytics?photographer=${user._id}`,
+        ),
+        api.get(`/images/get-images-by-photographer?photographer=${user._id}`),
+        api.get(
+          `/photographer/get-pending-images-by-photographer?photographer=${user._id}`,
+        ),
+        api.get(
+          `/catalogue/get-catalogues-by-photographer?photographer=${user._id}`,
+        ),
+        api.get(`/blog/get-my-blogs?author=${user._id}`),
+        api.get(`/blog/get-my-pending-blogs?author=${user._id}`),
+      ]);
+
+      if (statsRes.status === 'fulfilled') {
+        setStats(statsRes.value?.data);
+      } else {
+        setStats({});
+      }
+      if (photosRes.status === 'fulfilled') {
+        setPhotos(photosRes.value?.data?.photos);
+      } else {
+        setPhotos([]);
+      }
+      if (pendingPhotosRes.status === 'fulfilled') {
+        setPendingPhotos(pendingPhotosRes.value?.data?.pendingImages);
+      } else {
+        setPendingPhotos([]);
+      }
+      if (cataloguesRes.status === 'fulfilled') {
+        setCatalogues(cataloguesRes.value?.data?.catalogues);
+      } else {
+        setCatalogues([]);
+      }
+      if (blogsRes.status === 'fulfilled') {
+        setBlogs(blogsRes.value?.data?.blogs);
+      } else {
+        setBlogs([]);
+      }
+      if (pendingBlogsRes.status === 'fulfilled') {
+        setPendingBlogs(pendingBlogsRes.value?.data?.blogs);
+      } else {
+        setPendingBlogs([]);
+      }
+    } catch (error) {
+      console.error('Unexpected error fetching Profile data:', error);
+    } finally {
+      setLoading(false);
+    }
+  }, [user._id]);
+
+  const onRefresh = React.useCallback(async () => {
     setRefreshing(true);
     await useUserStore.getState().fetchUserFromToken();
     setRefreshing(false);
-  };
+  }, []);
 
   useEffect(() => {
     if (!user?._id) {
       return;
     }
     fetchAllData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
+  }, [user, fetchAllData]);
 
-  if (userLoading || refreshing) {
+  if (loading) {
     return (
       <SafeAreaView style={[style.background, {flex: 1}]}>
-        <ProfileSkeleton />
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator size={'large'} color="#ed3147" />
+        </View>
       </SafeAreaView>
     );
   }
 
-  const renderHeader = () => (
-    <MotiView
-      pointerEvents="box-none"
-      from={{opacity: 0.3}}
-      animate={{opacity: 1}}
-      transition={{type: 'timing', duration: 300}}
-      style={[{paddingBottom: 30}, style.background]}>
-      <View style={style.profileHeader}>
-        <View style={style.coverImageContainer}>
-          <Image
-            style={style.coverImage}
-            source={
-              user?.coverImage
-                ? {uri: user?.coverImage}
-                : require('../../assets/images/onboarding.png')
-            }
-          />
-          <View style={style.headerIcons}>
-            <Pressable
-              onPress={() => {
-                navigation.navigate('SettingsNavigator');
-              }}
-              style={style.iconContainer}>
-              <Icon name="gear" size={20} color={theme.text} />
-            </Pressable>
-            <Pressable
-              onPress={onShare}
-              title="Share"
-              style={style.iconContainer}>
-              <Icon name="share" size={20} color={theme.text} />
-            </Pressable>
-          </View>
-        </View>
-        <View style={style.profileDiv}>
-          <Image
-            style={style.profileImage}
-            source={
-              user?.profileImage
-                ? {uri: user?.profileImage}
-                : require('../../assets/images/onboarding.png')
-            }
-          />
-          <Pressable style={style.edit} onPress={() => setSlideUp(true)}>
-            <Image
-              style={{height: 12, width: 12, tintColor: theme.background}}
-              source={require('../../assets/tabIcons/edit.png')}
-            />
-          </Pressable>
-          <SlideUpModal
-            visible={slideUp}
-            onClose={() => setSlideUp(false)}
-            options={profileOptions}
-          />
-        </View>
-      </View>
-      <View style={style.userDetails}>
-        <Text style={style.userName}>
-          {user.firstName || ''} {user.lastName || ''}
-        </Text>
-        <Text style={style.userAddress}>
-          {user?.shippingAddress.city}, {user?.shippingAddress.country}
-        </Text>
-        <View style={{alignItems: 'center'}}>
-          <Text style={style.userBio}>
-            {fullBio
-              ? fullBio
-              : user?.bio?.split(' ').length <= 20
-              ? user?.bio
-              : user?.bio?.split(' ').slice(0, 20).join(' ') + '...'}
-          </Text>
-          {user?.bio?.split(' ').length > 20 && (
-            <Pressable
-              style={{
-                alignItems: 'center',
-                width: '90%',
-                paddingVertical: 16,
-                marginTop: -20,
-              }}
-              onPress={() => {
-                if (fullBio) {
-                  setFullBio(null);
-                } else {
-                  setFullBio(user?.bio);
-                }
-              }}>
-              <Icon
-                name={fullBio ? 'chevron-up' : 'chevron-down'}
-                size={20}
-                color={theme.text}
-              />
-            </Pressable>
-          )}
-        </View>
-      </View>
-      <View style={style.accountInfo}>
-        <View style={style.summary}>
-          <Text style={style.title}>IMPRESSIONS</Text>
-          <Text style={style.count}>{stats?.totalViews || 0}</Text>
-        </View>
-        <View style={style.summary}>
-          <Text style={style.title}>PHOTOS</Text>
-          <Text style={style.count}>{stats?.totalUploadingImgCount || 0}</Text>
-        </View>
-        <View style={style.summary}>
-          <Text style={style.title}>DOWNLOADS</Text>
-          <Text style={style.count}>{stats?.downloads || 0}</Text>
-        </View>
-      </View>
-    </MotiView>
-  );
-
   return (
     <SafeAreaView style={[style.background, {flex: 1}]}>
-      <Tabs.Container
-        renderHeader={renderHeader}
-        headerHeight={300}
-        allowHeaderOverscroll={true}
-        scrollEnabledHeader={true}
-        containerStyle={[style.tabsContainer, style.background]}
-        onTabChange={({tabName}) => setActiveTab(tabName.toLowerCase())}
-        headerContainerStyle={{backgroundColor: theme.background}}
-        renderTabBar={props => (
-          <MaterialTabBar
-            {...props}
-            labelStyle={{color: theme.text}}
-            indicatorStyle={{backgroundColor: '#ED3147'}}
-            activeColor={'#ED3147'}
-            inactiveColor={theme.text}
-            style={{backgroundColor: theme.background}}
+      <ScrollView
+        stickyHeaderIndices={[3]}
+        stickyHeaderHiddenOnScroll={true}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 30}}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing || profileUploading}
+            onRefresh={onRefresh}
+            tintColor="#000"
           />
-        )}>
-        <Tabs.Tab name="Photos">
-          <Tabs.ScrollView
-            onScroll={onScroll}
-            scrollEnabled={true}
-            scrollEventThrottle={16}
-            keyboardShouldPersistTaps="handled"
-            nestedScrollEnabled={true}
-            refreshControl={
-              <RefreshControl
-                refreshing={refreshing || profileUploading}
-                onRefresh={onRefresh}
-                tintColor="#000"
+        }>
+        <View style={style.profileHeader}>
+          <View style={style.coverImageContainer}>
+            <Image
+              style={style.coverImage}
+              source={
+                user?.coverImage
+                  ? {uri: user?.coverImage}
+                  : require('../../assets/images/onboarding.png')
+              }
+            />
+            <View style={style.headerIcons}>
+              <Pressable
+                onPress={() => {
+                  navigation.navigate('SettingsNavigator');
+                }}
+                style={style.iconContainer}>
+                <Icon name="gear" size={20} color={theme.text} />
+              </Pressable>
+              <Pressable
+                onPress={onShare}
+                title="Share"
+                style={style.iconContainer}>
+                <Icon name="share" size={20} color={theme.text} />
+              </Pressable>
+            </View>
+          </View>
+          <View style={style.profileDiv}>
+            <Image
+              style={style.profileImage}
+              source={
+                user?.profileImage
+                  ? {uri: user?.profileImage}
+                  : require('../../assets/images/onboarding.png')
+              }
+            />
+            <Pressable style={style.edit} onPress={() => setSlideUp(true)}>
+              <Image
+                style={{height: 12, width: 12, tintColor: theme.background}}
+                source={require('../../assets/tabIcons/edit.png')}
               />
-            }>  
-            <TabPhotos />
-          </Tabs.ScrollView>
-        </Tabs.Tab>
-        <Tabs.Tab name="Catalogues">
-          <Tabs.ScrollView
-            refreshControl={
-              <RefreshControl
-                refreshing={refreshing || profileUploading}
-                onRefresh={onRefresh}
-                tintColor="#000"
-              />
-            }>
-            <TabCatalogues />
-          </Tabs.ScrollView>
-        </Tabs.Tab>
-        <Tabs.Tab name="Blogs">
-          <Tabs.ScrollView
-            refreshControl={
-              <RefreshControl
-                refreshing={refreshing || profileUploading}
-                onRefresh={onRefresh}
-                tintColor="#000"
-              />
-            }>
-            <TabBlogs blogs={blogs} />
-          </Tabs.ScrollView>
-        </Tabs.Tab>
-      </Tabs.Container>
+            </Pressable>
+            <SlideUpModal
+              visible={slideUp}
+              onClose={() => setSlideUp(false)}
+              options={profileOptions}
+            />
+          </View>
+        </View>
+        <View style={style.userDetails}>
+          <Text style={style.userName}>
+            {user.firstName || ''} {user.lastName || ''}
+          </Text>
+          <Text style={style.userAddress}>
+            {user?.shippingAddress.city}, {user?.shippingAddress.country}
+          </Text>
+          <View style={{width: '100%', alignItems: 'center'}}>
+            <Text style={style.userBio}>
+              {fullBio
+                ? fullBio
+                : user?.bio?.split(' ').length <= 20
+                ? user?.bio
+                : user?.bio?.split(' ').slice(0, 20).join(' ') + '...'}
+            </Text>
+            {user?.bio?.split(' ').length > 20 && (
+              <Pressable
+                style={{paddingVertical: 16}}
+                onPress={() => {
+                  if (fullBio) {
+                    setFullBio(null);
+                  } else {
+                    setFullBio(user?.bio);
+                  }
+                }}>
+                <Icon
+                  name={fullBio ? 'chevron-up' : 'chevron-down'}
+                  size={20}
+                  color={theme.text}
+                />
+              </Pressable>
+            )}
+          </View>
+        </View>
+        <View style={style.accountInfo}>
+          <View style={style.summary}>
+            <Text style={style.title}>IMPRESSIONS</Text>
+            <Text style={style.count}>
+              {photos
+                ?.filter(photo => photo.imageAnalytics?.views)
+                .reduce(
+                  (acc, photo) => acc + (photo.imageAnalytics?.views || 0),
+                  0,
+                )}
+            </Text>
+          </View>
+          <View style={style.summary}>
+            <Text style={style.title}>PHOTOS</Text>
+            <Text style={style.count}>{stats.totalUploadingImgCount || 0}</Text>
+          </View>
+          <View style={style.summary}>
+            <Text style={style.title}>DOWNLOADS</Text>
+            <Text style={style.count}>{stats.downloads || 0}</Text>
+          </View>
+        </View>
+        <View style={style.stickyTabs}>
+          <View style={style.tabs}>
+            {tabs.map(tab => (
+              <TouchableOpacity
+                key={tab.key}
+                onPress={() => handleTabPress(tab.key)}>
+                <Text
+                  style={[
+                    style.tabText,
+                    activeTab === tab.key && {color: theme.text},
+                  ]}>
+                  {tab.label}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+        <View style={style.tabsContainer}>
+          <View>
+            {activeTab === 'photos' ? (
+              <TabPhotos photos={photos} pendingPhotos={pendingPhotos} />
+            ) : activeTab === 'catalogues' ? (
+              <TabCatalogues photos={photos} catalogues={catalogues} />
+            ) : (
+              <TabBlogs blogs={blogs} pendingBlogs={pendingBlogs} />
+            )}
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
